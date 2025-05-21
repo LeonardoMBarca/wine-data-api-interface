59247658b4eca989c7f2a027e5b8b0f295d6c945
Gabriel Lopes <gabriel.f.lopes@hotmail.com>
2025-05-21 20:20:31 -0300

    Adjustments sequence ETL

--- api/routes/crawler_routes.py
+++ api/routes/crawler_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required
 from crawler.scraper.downloader import download_base
+from pipelines.etl.bronze_to_silver.main import main as run_bronze_to_silver
+from pipelines.etl.silver_to_gold.main import main as run_silver_to_gold   
 
 crawler_bp = Blueprint("crawler", __name__)
 
@@ -9,6 +11,8 @@ crawler_bp = Blueprint("crawler", __name__)
 def executar():
     try:
         download_base()
+        run_bronze_to_silver() 
+        run_silver_to_gold()
         return jsonify({"mensagem": "Crawler executado com sucesso!"}), 200
     except Exception as e:
         return jsonify({"erro": f"Erro ao executar o crawler: {str(e)}"}), 500
